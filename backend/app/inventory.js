5d771b8f3e4dab826b8a39f3c9370d4d5d98fa69
Vitaliy Ten <vitalten10@gmail.com>
2021-10-23 10:40:49 +0600

    Выполнено добавление в базу

--- backend/app/inventory.js
+++ backend/app/inventory.js
@@ -22,39 +22,83 @@ router.get('/', async (req, res) => {
     res.send(inventory);
 });
 
-// router.get('/', (req, res) => {
-//     console.log('hello world');
-//     res.send('Hello');
-// });
+router.get('/:id', async (req, res) => {
+    const [inventory] = await mysql.getConnection().query(
+        'SELECT * FROM items where id = ?',
+        [req.params.id]);
+    res.send(inventory);
+});
 
-// router.get('/:id', (req, res) => {
-//     const product = fileDb.getItem(req.params.id);
-//     if (!product) {
-//         return res.status(404).send({error: 'Product not found'});
-//     }
-//
-//     res.send(product);
-// });
-//
-// router.post('/', upload.single('image'), (req, res) => {
-//     if (!req.body.title || !req.body.price || !req.body.description) {
-//         return res.status(400).send({error: 'Data not valid'});
-//     }
-//
-//     const product  = {
-//         title: req.body.title,
-//         price: req.body.price,
-//         description: req.body.description,
-//     };
-//
-//
-//     if (req.file) {
-//         product.image = req.file.filename;
-//     }
-//
-//     const newProduct = fileDb.addItem(product);
-//
-//     res.send(newProduct);
-// });
+// router()
+
+router.post('/', upload.single('image'), async (req, res) => {
+    if (!req.body.name || !req.body.location || !req.body.category || !req.body.description) {
+        return res.status(400).send({error: 'Data not valid'});
+    }
+
+    console.log(req.body.name);
+    console.log(req.body.location);
+    console.log(req.body.category);
+    console.log(req.body.description);
+
+    let location = null;
+    let category = null;
+
+    switch (req.body.location) {
+        case 'Office':
+            location = 2;
+            break;
+        case 'Head office':
+            location = 1;
+            break;
+        case 'Back office':
+            location = 3
+            break;
+        default:
+            break;
+    };
+
+    switch (req.body.category) {
+        case 'Furniture':
+            category = 1;
+            break;
+        case 'Office equipment':
+            category = 2;
+            break;
+        case 'Other':
+            category = 3
+            break;
+        default:
+            break;
+    };
+
+    console.log('Category: ', category);
+    console.log('Location: ', location);
+
+
+    const item  = {
+        category_id: category,
+        location_id: location,
+        name: req.body.name,
+        description: req.body.description,
+    };
+
+    console.log(item);
+
+
+    if (req.file) {
+        item.image = req.file.filename;
+    }
+
+    const newItem = await mysql.getConnection().query(
+        'INSERT INTO ?? (category_id, location_id, name, description, photo) values (?, ?, ?, ?, ?)',
+        ['items', item.category_id, item.location_id, item.name, item.description, item.image]
+    );
+
+    res.send({
+        ...item,
+        id: newItem.insertId
+    });
+});
 
 module.exports = router;
